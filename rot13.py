f5ae6184b422d1b3390ce66e2bf4b33b8412b2cc
0days0 <0days0@github.com>
2020-06-17 00:24:07 +0300

    Create rot13.py

---
+++ rot13.py
@@ -0,0 +1,20 @@
+print("""
+##########################
+##### @author 0days0 #####
+##### ROT13 Chipher  #####
+##########################""")
+# 10.04.2020
+alfabe = str.maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', 
+                       'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm')
+
+def rot13(yazi):
+    return yazi.translate(alfabe)
+
+def cümle():
+    yazi = input("Şifrelemek istediğiniz cümleyi giriniz('Türkçe karakterler hariç'): ")
+  # yazi = yazi.lower() (şifrelenen harfleri küçültür)
+    print("Şifreli metniniz: ", rot13(yazi))
+
+cümle()
+
+
